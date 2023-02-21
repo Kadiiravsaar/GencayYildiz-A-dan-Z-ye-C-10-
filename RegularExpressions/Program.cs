a2c858c6c3337f410745f43b2ba133cdc4bbb850
kadiiravsaar <kadiiravsar@gmail.com>
2023-02-21 11:44:47 +0300

    ..

--- RegularExpressions/Program.cs
+++ RegularExpressions/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 #region Ders 346 ^ Operatörü
 
 // ^ Operatörü : Satır başının istenen ifade ile başlamsını sağlar.
@@ -33,14 +36,96 @@
 // ^9\d\S
 
 
-string text = "92kakaskas544asdlasdls1212";
+//string text = "92kakaskas544asdlasdls1212";
 
-Regex regex = new Regex("^9\\d\\S"); // başlamsını istediğimiz ifade nesne ürettikten sonra buraya yazılır
-//Regex regex = new Regex(@"^9\d\S"); // İkinci seçenek
-Match match = regex.Match(text);
+//Regex regex = new Regex("^9\\d\\S"); // başlamsını istediğimiz ifade nesne ürettikten sonra buraya yazılır
+////Regex regex = new Regex(@"^9\d\S"); // İkinci seçenek
+//Match match = regex.Match(text);
 
-Console.WriteLine(match.Success); // doğru ise true dönecektir.
+//Console.WriteLine(match.Success); // doğru ise true dönecektir.
 
 #endregion
+#region Ders 348 + Operatörü
+
+// Belirtien gruptaki karakterlerden bir ya da daha fazlasının olmasını istiyorsak kullanırız
+
+//9 ile başlayan arada  herhangi bir sayısal değerleri olan ve son karakteri de boşluk olmayan bir düzenli ifade oluşturalım 
+
+// ^9\d+\S
+
+//string text = "98745263d";
+
+//Regex regex = new Regex(@"^9\d+\S");
+//Match match = regex.Match(text);
+
+//Console.WriteLine(match.Success);
+
+
+#endregion
+#region | Operatörü
+//Birden fazla karakter grubundan bir ya da birkacinin ilgili yerde olabilecegini belirtmek istiyorsak mantiksal
+//veya operatérii kullanilir.
+
+
+// Bas harfi a ya da b ya da c olan metinsel ifadeyi girelim.
+// a|b|c
+
+//string text = "Ahmet";
+//Regex regex = new Regex(@"a|b|c");// büyük küçük harf duyarlıdır.
+//Match match = regex.Match(text);
+
+//Console.WriteLine(match.Success);
+
+
+#endregion
+#region {n} Operatörü
+
+//Sabit sayida karakterin olmasa isteniyorsa {adet} seklinde belirtilmeli.
+
+//5@7-7514592
+//\d\d\d-\d\d\d\d\d\d
+// \d{3}-\d{6}
+
+//string text = "507-751459";
+//Regex regex = new Regex(@"\d{3}-\d{6}");
+//Match match = regex.Match(text);
+
+//Console.WriteLine(match.Success);
+
+#endregion
+#region ? Operatörü
+
+// Bu karakterin önüne gelen karakter en efazla bir, en az sıfır defa olmalıdır.
+
+//\d{3}B?A -----> 213BA, 123A
+
+//string text = "123A";
+//Regex regex = new Regex(@"\d{3}B?A");
+//Match match = regex.Match(text);
+
+//Console.WriteLine(match.Success);
+
+#endregion
+#region . Operatörü
+
+//\n = Kullanildigi yerde \n karakteri disinda herhangi bir karakter bulunabilir.
+
+
+//string text = "123\hA";
+//Regex regex = new Regex(@"\d{3}.A");
+//Match match = regex.Match(text);
+
+//Console.WriteLine(match.Success);
+
+#endregion
+
+
+
+
+
+
+
+
+
 
 
